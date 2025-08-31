c44646faf458c1cb9f53d0f8c8679127159ba0a3
mpinchover <mpinchover@gmail.com>
2025-08-31 16:48:02 -0700

    added in images and created home page

--- src/components/home-logged-out/index.jsx
+++ src/components/home-logged-out/index.jsx
@@ -1,7 +1,32 @@
 "use client";
-import { VStack, Link, Image, Text, Button } from "@chakra-ui/react";
+import {
+  VStack,
+  Link,
+  Image,
+  Text,
+  Button,
+  Box,
+  Heading,
+} from "@chakra-ui/react";
+import image1 from "./image1.png";
+import image2 from "./image2.png";
+import image3 from "./image3.png";
+import image4 from "./image4.png";
+import image5 from "./image5.png";
+import image6 from "./image6.png";
+import image7 from "./image7.png";
+import { useRouter } from "next/navigation";
+
+const BGImage = ({ src }) => {
+  return (
+    <Box borderRadius="md" overflow="hidden" height="150px" width="100%">
+      <Image filter="saturate(50%)" src={src} />
+    </Box>
+  );
+};
 
 const HomeLoggedOut = () => {
+  const router = useRouter();
   return (
     <VStack
       paddingBottom="80px"
@@ -17,8 +42,22 @@ const HomeLoggedOut = () => {
         alignItems="start"
         position="relative"
       >
-        <Text>A better way to connect.</Text>
-        <Link>Get started here.</Link>
+        <Heading fontWeight="400" size="xl">
+          Does this give you{" "}
+          <Text as="span" fontWeight="extrabold">
+            diarrhea
+          </Text>
+          ?
+        </Heading>
+        {/* <Text>A better way to connect.</Text>
+        <Link>Get started here.</Link> */}
+        <BGImage src={image6.src} />
+        <BGImage src={image1.src} />
+        {/* <BGImage src={image2.src} /> */}
+        <BGImage src={image3.src} />
+        <Link onClick={() => router.push("/login")}>
+          Log in and share your profile without needing a toilet.
+        </Link>
       </VStack>
     </VStack>
   );
